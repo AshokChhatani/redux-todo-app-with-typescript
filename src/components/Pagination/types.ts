55db890acb553d2c4b40d9efb09c52ea2c8d2a1d
Ashok <chhataniashok@gmail.com>
2022-06-17 17:46:35 +0530

    .

---
+++ src/components/Pagination/types.ts
@@ -0,0 +1,8 @@
+export type PaginationProps = {
+  onPageChange: Function;
+  totalCount: number;
+  siblingCount: any;
+  currentPage: number;
+  pageSize: number;
+  className: string;
+};
--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -1,18 +1,38 @@
-import { useEffect } from "react";
+import { useEffect, useMemo, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { add } from "../store/todoSlice";
 import { getlistFetch } from "../store/tasklistSlice";
 import { STATUSES } from "../store/tasklistSlice";
 import { Task, TaskList as TaskListType } from "../store/types";
 import { RootStateType } from "../store/store";
+import { Box, Paper, TextField } from "@mui/material";
+import { ChangeEvent } from "react";
+import Pagination from "./Pagination/Pagination";
 
 const TaskList = () => {
   const dispatch = useDispatch();
+
   const { tasklist, status } = useSelector(
     (state: RootStateType) => state.tasklist
   ) as TaskListType;
 
   const todo = useSelector((state: RootStateType) => state.todo) as Task[];
+
+  const [list, setList] = useState(tasklist);
+
+  const [currentPage, setCurrentPage] = useState(1);
+
+  let PageSize = 12;
+
+  const currentData = useMemo(() => {
+    const firstPageIndex = (currentPage - 1) * PageSize;
+    const lastPageIndex = firstPageIndex + PageSize;
+    return list.slice(firstPageIndex, lastPageIndex);
+  }, [currentPage, PageSize, list]);
+
+  useEffect(() => {
+    setList(tasklist);
+  }, [tasklist]);
 
   useEffect(() => {
     dispatch(getlistFetch());
@@ -38,20 +58,59 @@ const TaskList = () => {
   if (status === STATUSES.ERROR) {
     return <h2>Something went wrong!</h2>;
   }
-  return (
-    <div className="wrapper">
-      {tasklist.map((task) => (
-        <div key={task.id}>
-          <div className="card" key={task.id}>
-            <h4>{task.title}</h4>
 
-            <button onClick={() => handleAdd(task)} className="btn">
-              Add
-            </button>
+  const searchHandler = (event: ChangeEvent<HTMLInputElement>) => {
+    const value = event.target.value;
+    const list = tasklist.filter((task) => task.title.includes(value));
+    setList(list);
+  };
+
+  return (
+    <>
+      <Box
+        component="form"
+        sx={{
+          "& .MuiTextField-root": { m: 0, width: "100%" },
+        }}
+        noValidate
+        autoComplete="off"
+      >
+        <Paper variant="outlined">
+          <TextField
+            fullWidth
+            id="outlined-basic"
+            label="Search"
+            variant="outlined"
+            onChange={searchHandler}
+          />
+        </Paper>
+      </Box>
+
+      <div className="wrapper" style={{ marginTop: "10px" }}>
+        {currentData.map((task) => (
+          <div key={task.id}>
+            <div className="card" key={task.id}>
+              <h4>{task.title}</h4>
+
+              <button onClick={() => handleAdd(task)} className="btn">
+                Add
+              </button>
+            </div>
           </div>
-        </div>
-      ))}
-    </div>
+        ))}
+      </div>
+      <div className="pagination">
+        <Pagination
+          siblingCount
+          className="pagination-bar"
+          currentPage={currentPage}
+          totalCount={list.length}
+          pageSize={PageSize}
+          onPageChange={(page: number) => setCurrentPage(page)}
+        />
+      </div>
+      {list.length === 0 && <h2>Task not found</h2>}
+    </>
   );
 };
 
